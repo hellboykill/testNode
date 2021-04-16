d88a8559f2256647f076128c2b5fef710538a095
hellboykill <badboy1998hh@gmal.com>
2021-04-16 15:56:31 +0700

    commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
+
+var authMiddleware = require('./middlewares/auth.middleware');
+
+var port = 3000;
+
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser('process.env.SESSION_SECRET'));
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        name: 'Hello'
+    });
+});
+
+app.use('/users', authMiddleware.requireAuth, userRoute);
+app.use('/auth', authRoute);
+
+app.listen(port, function() {
+    console.log('Sever listening on port ' + port);
+});
